241339e46a31e4d84c63ea4abb63c609444fa196
cetecom <cetecom@LC08L1313>
2016-03-28 20:16:47 -0300

    avance 2.5 a 3.0

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php 
+        require 'variables.php';
+        ?>
+
+<!DOCTYPE html>
+<!--
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title><?php echo $titulo;?></title>
+    </head>
+    <link type="text/css" rel="stylesheet" href="style.css" >
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+    <body>
+        
+        <?php 
+        require 'menu.php';
+        ?>
+        <br>
+        
+        
+        </body>
+</html>
---
+++ menu.php
@@ -0,0 +1,17 @@
+
+<titulo>HORMIGON300</titulo>
+<?php
+
+echo '
+    
+<ul>
+    <img src="http://www.hormigon.com.mx/img/logo.png" alt="logo" >
+    <li><a href="index.php">Inicio</a></li>
+    <li><a href="#">Bodega</a></li>
+    <li><a href="avance.php">Avances</a></li>
+    <li><a href="#">Compras</a></li>
+    <li><a href="resumen.php">Resumen</a></li>
+</ul>
+
+';
+?>
